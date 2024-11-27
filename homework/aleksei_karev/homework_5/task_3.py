9d1ff806a92887d3dcef92d8a067310ddcdecf62
Aleksey Karev <laurent.94@yandex.ru>
2024-11-28 00:16:08 +0100

    feat: homework_5 done

---
+++ homework/aleksei_karev/homework_5/task_3.py
@@ -0,0 +1,5 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+student_text = f'Students {students[0]}, {students[1]}, {students[2]} study these '
+subjects_text = f'subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}'
+print(student_text + subjects_text)

82788e8e0e39f584888742d5f42aa811c0ce3825
Aleksey Karev <laurent.94@yandex.ru>
2024-11-28 23:27:27 +0100

    fix: replace index-based slicing with join

--- homework/aleksei_karev/homework_5/task_3.py
+++ homework/aleksei_karev/homework_5/task_3.py
@@ -1,5 +1,6 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-student_text = f'Students {students[0]}, {students[1]}, {students[2]} study these '
-subjects_text = f'subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}'
-print(student_text + subjects_text)
+str_students = ', '.join(students)
+str_subjects = ', '.join(subjects)
+text = f'Students {str_students} study these subjects: {str_subjects}'
+print(text)
